95e6a1c4e95b8d77e7e7e62dfb1ff092d9e860a2
Jitters <ryan_walton5@eku.edu>
2022-09-14 22:38:21 -0400

    Second commit for some reason

---
+++ JavaPractice/JavaCheatSheet/src/CheatSheet.java
@@ -0,0 +1,22 @@
+public class CheatSheet {
+    public static void main(String[] args) throws Exception {
+        //variables
+        int a = 2;
+        double b = 3.333;
+        String c = "Words."; //Remember to capitalize "String"
+        Boolean isTrue = true; //Same here
+        System.out.println("Use this to print to terminal, " + c);
+        System.out.println(a + b);
+        System.out.println(isTrue);
+        System.out.println("You could print " + a + " and " + b + " like this.  Or with System.out.print");
+        System.out.print(a);  //System.out.print will print stuff on the same line.  Println prints everything on a new line.
+        System.out.print(b);
+        System.out.print(c);
+        System.out.print(isTrue);
+
+        /* There are lots of different variables for different sizes of data.
+        Long is a datatype that holds huge numbers, but is kind of the same as a double (which doesn't hold as much as long) */
+
+        int[] myNumbers = {1, 2, 3}; //Makes an array.
+    } 
+}
