8cbbc679dc6ca7cb37d2ff208df60e5f22db972a
Jitters <ryan_walton5@eku.edu>
2022-09-16 13:51:36 -0400

    Fixed App.java practice code.  launch.json?

--- JavaPractice/JavaPracticeCode/src/App.java
+++ JavaPractice/JavaPracticeCode/src/App.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class App {
+    public static Scanner purpose;
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World!  What is my purpose?");
+            purpose = new Scanner(System.in);
+            String meaning = purpose.nextLine();
+            System.out.println("I've always wanted " + meaning);
     }
 }
