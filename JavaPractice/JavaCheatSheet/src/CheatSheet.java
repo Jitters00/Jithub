760379ac2374898bc30e61794b4290aec7953d66
Jitters <ryan_walton5@eku.edu>
2022-09-18 11:56:35 -0400

    JavaCheatSheet

--- JavaPractice/JavaCheatSheet/src/CheatSheet.java
+++ JavaPractice/JavaCheatSheet/src/CheatSheet.java
@@ -10,19 +10,14 @@ public class CheatSheet {
         System.out.println(a + b);
         System.out.println(isTrue);
         System.out.println("You could print " + a + " and " + b + " like this.  Or with System.out.print");
-        System.out.println("System.out.print without println returns values on the same line (remember to include spaces):")
-        System.out.print(a, " ");  //System.out.print will print stuff on the same line.  Println prints everything on a new line.
-        System.out.print(b, " ");
-        System.out.print(c, " ");
-        System.out.print(isTrue " ");
+        System.out.println("System.out.print returns values on the same line:");
+        System.out.print(a + " ");  
+        System.out.print(b + " ");  //print will print variables on the same line.  Println includes a line break at the end of its output.
+        System.out.println(c + " "); //Note the spaces.
+        System.out.println(isTrue);
 
-        /* There are lots of different variables for different sizes of data.
-        Long is a datatype that holds huge numbers, but is kind of the same as a double
-        (which doesn't hold as much as long).  Int and Double are the most common, I'm told.*/
-
-        System.out.println()
         int[] myNumbers = {1, 2, 3}; //Makes an array.
-        char[] myCharacters = {a, b, c, d};
-        System.out.println(myCharacters[0], myNumbers[2]); //Access the variables in your array like this, starting with 0.
+        String[] myCharacters = {"Jack", "Bob", "Tim"};
+        System.out.println(myCharacters[0] + " " + myNumbers[2]); //Access the variables in your array like this, starting with 0.
     } 
 }
