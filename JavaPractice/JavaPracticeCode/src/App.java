5deb5f2ffc1ca4dc5ad5cccaac2a55fdbd6d274e
Jitters <ryan_walton5@eku.edu>
2022-09-18 19:48:32 -0400

    More notes on app.

--- JavaPractice/JavaPracticeCode/src/App.java
+++ JavaPractice/JavaPracticeCode/src/App.java
@@ -4,13 +4,18 @@ public class App {
     public static void main(String[] args) throws Exception {
         //This code will display a list of meals based on the list of ingredients provided by the user.
 
-        /*
+        /* 
          * main:
          * user inputs ingredients
          * input ingredients into an array which is cleared each time the app is used
          * the tempArray is compared against meal and side arrays.
-         * once all conditions for a entre or side are met, they are displayed. 
+         * once all conditions for a entre or side are met, they are displayed.
+         * 
+         * class:
+         * user enters necessary ingredients into arrays for meals and sides
+         * input name of array
+         * input ingredients
+         * break operation
          */
-
     }
 }
