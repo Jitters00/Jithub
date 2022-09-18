9e1c11ddaa80a079e04c6076683e1c5c7a4455ad
Jitters <ryan_walton5@eku.edu>
2022-09-18 11:25:57 -0400

    Added to JavaCheatSheet from desktop.  Setup git.

--- JavaPractice/JavaCheatSheet/src/CheatSheet.java
+++ JavaPractice/JavaCheatSheet/src/CheatSheet.java
@@ -5,18 +5,24 @@ public class CheatSheet {
         double b = 3.333;
         String c = "Words."; //Remember to capitalize "String"
         Boolean isTrue = true; //Same here
-        System.out.println("Use this to print to terminal, " + c);
+        int emptyValue; //Declared without a value assigned.
+        System.out.println("Use this to print to terminal");
         System.out.println(a + b);
         System.out.println(isTrue);
         System.out.println("You could print " + a + " and " + b + " like this.  Or with System.out.print");
-        System.out.print(a);  //System.out.print will print stuff on the same line.  Println prints everything on a new line.
-        System.out.print(b);
-        System.out.print(c);
-        System.out.print(isTrue);
+        System.out.println("System.out.print without println returns values on the same line (remember to include spaces):")
+        System.out.print(a, " ");  //System.out.print will print stuff on the same line.  Println prints everything on a new line.
+        System.out.print(b, " ");
+        System.out.print(c, " ");
+        System.out.print(isTrue " ");
 
         /* There are lots of different variables for different sizes of data.
-        Long is a datatype that holds huge numbers, but is kind of the same as a double (which doesn't hold as much as long) */
+        Long is a datatype that holds huge numbers, but is kind of the same as a double
+        (which doesn't hold as much as long).  Int and Double are the most common, I'm told.*/
 
+        System.out.println()
         int[] myNumbers = {1, 2, 3}; //Makes an array.
+        char[] myCharacters = {a, b, c, d};
+        System.out.println(myCharacters[0], myNumbers[2]); //Access the variables in your array like this, starting with 0.
     } 
 }
