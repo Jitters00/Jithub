1df911dfcb58b7227afb8bbf2ac0f17996fcb3e7
Jitters <ryan_walton5@eku.edu>
2022-09-23 21:49:15 -0400

    New?

--- JavaPractice/JavaPracticeCode/src/SetListApp.java
+++ JavaPractice/JavaPracticeCode/src/SetListApp.java
@@ -1,40 +1,38 @@
+//Set List.
+/* 
+ * main:
+ * requests user input.
+ * if user input is blank, print random song title.
+ * if user input is not blank, add song title to the list.
+ * loop back to beginning.
+ * 
+ */
+
 import java.util.Scanner;
 import java.util.ArrayList;
 
-public class App {
+public class SetListApp {
     public static void main(String[] args){
-        //Set List.
-        /* 
-         * main:
-         * write random line from list.
-         * add new lines to list.
-         * link lines to websites.
-         * 
-         * import scanner.
-         * import array list.
-         * while loop < 1 {
-         * write line from array list.
-         * request user input.
-         * add user input to array list.
-         * ++loop.
-         * }
-         * on press, --loop.  This one might have to be contained in some other sort of loop so the program doesn't end.
-         */
-        Scanner newSong = new Scanner(System.in);
         ArrayList<String> knownSongs = new ArrayList<String>();
-        int runCondition;
-
-        String song = newSong.nextLine();
-        if (song = "") {
-            while (song = "") {
-                System.out.println(knownSongs.get(0)); //print song title.  There's likely a need for more code here. math.random wont cut it.
+        Scanner newSong = new Scanner(System.in);
+            for ( ; ; ){
+            String song = newSong.nextLine();
+            switch (song) {
+                case "":
+                System.out.println(knownSongs.get(0 + 1));               //??? Try this.
+                break;
+                default:
+                System.out.println("uhhhh");
+                knownSongs.add(song);
+                System.out.println(knownSongs.size());
+                System.out.println(knownSongs.get(0));
+                break;
             }
-        }
-        else {
-            knownSongs.add(song); //add song
         }
     }
 }
+
+
 
 //prevent user from adding songs more than once.
 //implement removing songs.
